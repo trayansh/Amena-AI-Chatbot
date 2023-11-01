4f21172f30d318bda52784f323f5973645511d09
Trayansh Dewangan <68157983+trayansh@users.noreply.github.com>
2023-11-01 15:37:24 +0530

    Create build.sh
    build file

---
+++ build.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python3-pip python-dev
+sudo apt-get install ffmpeg
+pip install gradio==3.38.0 --use-deprecated=legacy-resolver
+pip install --upgrade google-cloud-speech==2.21.0
+pip install torch
+pip install speechrecognition
+pip install pyaudio
+gradio main.py
+
